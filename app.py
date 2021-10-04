e27191878b228fa2182cb43bf214b5945173953e
Knowledge Shelf <60973040+knowledgeshelfit@users.noreply.github.com>
2021-10-04 10:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 import streamlit as st
 from streamlit_webrtc import VideoProcessorBase, webrtc_streamer, RTCConfiguration,WebRtcMode
 
+
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
@@ -20,12 +21,26 @@ class VideoTransformer(VideoProcessorBase):
 
         return img
 
+class CameraFrame(VideoProcessorBase):
+
+    def camera(self, frame):
+        cam = frame.to_ndarray(format="bgr24")
+
+        return cam
+
+
 playing = st.checkbox("Playing", value=True)
 
-ctx = webrtc_streamer(key="media-constraints", video_transformer_factory=VideoTransformer,
+camf = webrtc_streamer(key="media-constraints1", video_processor_factory=CameraFrame,
                         desired_playing_state=playing,
                         mode=WebRtcMode.SENDRECV,
                         rtc_configuration=RTC_CONFIGURATION)
+
+ctx = webrtc_streamer(key="media-constraints2", video_processor_factory=VideoTransformer,
+                        desired_playing_state=playing,
+                        mode=WebRtcMode.SENDRECV,
+                        rtc_configuration=RTC_CONFIGURATION)
+
 
 if ctx.video_transformer:
     ctx.video_transformer.threshold1 = st.slider("Threshold1", 0, 1000, 100)
